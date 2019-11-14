4c69c506df1cf9d088e9acd987fef41aec48e397
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2019-11-14 12:06:42 +0700

    พำ

--- bot.php
+++ bot.php
@@ -65,11 +65,34 @@ if(!is_null($events)){
     switch ($typeMessage){
         case 'text':
             switch ($userMessage) {
-                case "A":
-                    $textReplyMessage = "คุณพิมพ์ A";
+                case "เบอร์ติดต่อแต่ละสาขา":
+                    $textReplyMessage = "สาขาแจ้งวัฒนะ 2533"."\n"."สาขาหนองแขม 3533"."\n"."สาขาบางกอกน้อย 4533"."\n"."สาขาหนองแขม 3533";
                     break;
                 case "B":
                     $textReplyMessage = "คุณพิมพ์ B";
+                    break;
+                case (preg_match('/^create-/',$userMessage) ? true : false): // เมื่อพิมพ์คำว่า create- เข้ามา
+                    $respRichMenu = $bot->createRichMenu(
+                            new RichMenuBuilder(
+                                new RichMenuSizeBuilder(1686,2500), // ขนาด rich menu ปกติจะไม่เปลี่ยน แปลง
+                                true, // เปิดให้แสดง * จะไม่แสดงทันที 
+                                "Rich Menu 1", // ชื่อ rich menu
+                                "เมนู", // ข้อความที่จะแสดงที่แถบเมนู
+                                array( // array ของ action แต่ละบริเวณ
+                                    new RichMenuAreaBuilder( // action ที่ 1
+                                        new RichMenuAreaBoundsBuilder(0,0,1250,1686),// พื้นที่ A (x,y,width,height)
+                                        new MessageTemplateActionBuilder('m','Area A') // เปลี่ยนเฉพาะตัวที่ 2 ตามต้องการ 'Area A'
+                                    ),
+                                    new RichMenuAreaBuilder( // action ที่ 2
+                                        new RichMenuAreaBoundsBuilder(1250,0,1250,1686), // พื้นที่ B (x,y,width,height)
+                                        new UriTemplateActionBuilder('u','http://niik.in') // เปลี่ยนเฉพาะตัวที่ 2 ตามต้องการ 'http://niik.in'
+                                    ),                                                                                  
+                                )
+                            )
+                        );          
+                        // ให้ bot แจ้งกลับเกี่ยวกับ สถานะการสร้าง
+                        $textReplyMessage = " การสร้าง Rich Menu ".$respRichMenu->getRawBody();
+                        $replyData = new TextMessageBuilder($textReplyMessage);                                                     
                     break;
                 default:
                     $textReplyMessage = " คุณไม่ได้พิมพ์ A และ B";
